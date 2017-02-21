d3e23f638f1748571c42c2703a1aa3e650911432
jessicalam <j.lam@mail.com>
2017-02-21 17:46:54 -0500

    first commit

---
+++ localstore.js
@@ -0,0 +1,33 @@
+var time = document.getElementById('time')
+var storage = window.localStorage.getItem('storage')
+function change () {
+  if (time.className === 'day-mode') {
+    time.setAttribute('class', 'night-mode')
+    storage = 'night-mode'
+    window.localStorage.setItem('storage', storage)
+  } else {
+    time.setAttribute('class', 'day-mode')
+    storage = 'day-mode'
+    window.localStorage.setItem('storage', storage)
+  }
+}
+
+window.onload = function () {
+  document.getElementById('button').addEventListener('click', change)
+  time.setAttribute('class', storage)
+}
+
+window.localStorage.setItem('storage', storage)
+
+
+//FOR THE COUNTER ELEMENT
+var counter = window.localStorage.getItem('counter')
+if (counter === null) {
+  counter = 1
+} else {
+  counter = parseInt(counter) + 1
+}
+window.localStorage.setItem('counter', counter)
+
+var counterElement = document.getElementById('counter')
+  counterElement.textContent = 'Counter : ' + counter
